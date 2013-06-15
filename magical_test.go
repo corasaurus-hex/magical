9e8e8b010fe94b9d931bd2a11f5ea95ae239e44e
Nathan Sutton <nate@zencoder.com>
2013-06-15 18:44:16 -0500

    Get hardware address as an integer

---
+++ magical_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"testing"
+	"net"
+)
+
+func TestGetHardwareAddress(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Could not get hardware address: %v", r)
+		}
+	}()
+
+	var hardwareAddress net.HardwareAddr = getHardwareAddress()
+
+	if len(hardwareAddress) == 0 {
+		t.Errorf("Got a bad hardware address: %v", hardwareAddress)
+	}
+}
+
+func TestGetHardwareAddressAsUint64(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Could not get hardware address as an integer: %v", r)
+		}
+	}()
+
+	var hardwareAddressAsUint64 uint64 = getHardwareAddressAsUint64()
+
+	if hardwareAddressAsUint64 < 1 {
+		t.Errorf("Could not get the hardware address as an integer: %v", hardwareAddressAsUint64)
+	}
+}
+
+func TestConvertHardwareAddressToUint64WithGoodValue(t *testing.T) {
+	defer func() {
+		if r := recover(); r != nil {
+			t.Errorf("Could not convert net.HardwareAddr to integer: %v", r)
+		}
+	}()
+
+	hardwareAddr, _ := net.ParseMAC("34:B6:02:61:DE:1B")
+
+	uintHardwareAddr := convertHardwareAddressToUint64(hardwareAddr)
+
+	if uintHardwareAddr != 57956328660507 {
+		t.Errorf("Expected %v == %v", "34:B6:02:61:DE:1B", 57956328660507)
+	}
+}
