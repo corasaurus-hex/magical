94907f4fff1228594dd40ba8655bea8feffa04a9
Nathan Sutton <nate@zencoder.com>
2013-06-15 18:56:59 -0500

    Putting things back because you can't do that with go (also added getMilliseconds)

--- magical.go
+++ magical.go
@@ -3,12 +3,55 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
+	"strings"
+	"strconv"
+	"time"
 )
 
 func main() {
+	fmt.Println(getMilliseconds())
 	fmt.Println(getHardwareAddressAsUint64())
 }
 
 func logFatalErrorf(format string, a ...interface{}) {
 	log.Fatal(fmt.Errorf(format, a...))
+}
+
+func getHardwareAddressAsUint64() (uintHardwareAddr uint64) {
+	return convertHardwareAddressToUint64(getHardwareAddress())
+}
+
+func convertHardwareAddressToUint64(hardwareAddress net.HardwareAddr) (uintHardwareAddr uint64) {
+	strippedHardwareAddr := strings.Replace(hardwareAddress.String(), ":", "", -1)
+
+	uintHardwareAddr, err := strconv.ParseUint(strippedHardwareAddr, 16, 48)
+
+	if err != nil {
+		logFatalErrorf("Unable to parse %q as an integer: %q", strippedHardwareAddr, err)
+	}
+
+	return uintHardwareAddr
+}
+
+func getHardwareAddress() (net.HardwareAddr) {
+	interfaces, err := net.Interfaces()
+
+	if err != nil {
+		logFatalErrorf("Could not get any network interfaces", err)
+	}
+
+	for _, value := range interfaces {
+		if len(value.HardwareAddr) > 0 {
+			return value.HardwareAddr
+		}
+	}
+
+	logFatalErrorf("No interface found with a MAC address: %v", interfaces)
+
+	return nil
+}
+
+func getMilliseconds() uint64 {
+	return uint64(time.Now().UnixNano() / 1e6)
 }
