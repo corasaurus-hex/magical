64a887aefb9d0e883a23fc875129cacecaa4abcb
Cora Sutton <cora@sutton.me>
2013-06-18 01:41:00 -0500

    Generate some ids

--- magical.go
+++ magical.go
@@ -3,11 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/big"
 	"net"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
-	"math/big"
 )
 
 const (
@@ -15,8 +16,21 @@ const (
 	sequenceBits   = uint(16)
 )
 
+var (
+	timeInMs     uint64
+	hardwareAddr uint64
+	sequence     uint64
+	mutex        *sync.Mutex
+)
+
 func main() {
-	fmt.Println(nextId())
+	timeInMs = getTimeInMilliseconds()
+	hardwareAddr = getHardwareAddrAsUint64()
+	sequence = 0
+	mutex = new(sync.Mutex)
+	for i := 0; i < 1000; i++ {
+		fmt.Printf("New value: %v\n", nextId())
+	}
 }
 
 func getHardwareAddrAsUint64() (uintHardwareAddr uint64) {
@@ -56,12 +70,8 @@ func getHardwareAddr() net.HardwareAddr {
 	return nil
 }
 
-func milliseconds() uint64 {
+func getTimeInMilliseconds() uint64 {
 	return uint64(time.Now().UnixNano() / 1e6)
-}
-
-func sequence() uint64 {
-	return uint64(0)
 }
 
 func mergeNumbers(now uint64, mac uint64, seq uint64) string {
@@ -71,9 +81,21 @@ func mergeNumbers(now uint64, mac uint64, seq uint64) string {
 	i.Or(new(big.Int).SetUint64(mac), i)
 	i.Lsh(i, sequenceBits)
 	i.Or(new(big.Int).SetUint64(seq), i)
-	return i.String()
+	return fmt.Sprintf("%x", i)
 }
 
 func nextId() string {
-	return mergeNumbers(milliseconds(), getHardwareAddrAsUint64(), sequence())
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	newTimeInMs := getTimeInMilliseconds()
+
+	if newTimeInMs == timeInMs {
+		sequence += 1
+	} else {
+		timeInMs = newTimeInMs
+		sequence = 0
+	}
+
+	return mergeNumbers(timeInMs, hardwareAddr, sequence)
 }
